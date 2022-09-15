1d73169955033e6e1d480a2236e4b65d9cccf374
SadeEtensh <eteneshsade550305@gmail.com>
2022-09-15 16:41:10 +0100

    complted

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://afbd6d66e03ca4b78b6fb5feec204cd3-1580960835.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://0eb2d9c83f30463391a87c6ca4368db-201688052.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
